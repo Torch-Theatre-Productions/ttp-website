f53ec8eca266d791ed742d168c9a86554884009b
Chris J <5574633+frisbee09@users.noreply.github.com>
2024-08-16 22:37:57 +0100

    Trying to fix webpack unexpected character issues

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,8 @@
 exports.onCreateWebpackConfig = ({ actions }) => {
   return actions.setWebpackConfig({
+    stats:{
+      errorDetails: true
+    },
     resolve: {
       fallback: {
         path: require.resolve("path-browserify"),
