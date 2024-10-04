a972a86e6ba7f4769a9d5e6a901c57360cc102f7
Chris Johnstone <5574633+frisbee09@users.noreply.github.com>
2024-10-05 00:25:27 +0100

    Fleshing out the OD Structure and content

---
+++ src/pages/shows/ordinary-days/components/OrdinaryDaysSection.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import { theme } from "../../components/.theme";
+
+export const OrdinaryDaysSection = styled.div`
+  // min-height: calc(2 * ${theme.padding.top}rem);
+  // // Pad the top and bottom
+  // padding-top: ${theme.padding.top * 2}rem;
+  // padding-bottom: ${theme.padding.top * 2}rem;
+
+  // &:nth-of-type(2n - 1) {
+  //   background: white;
+
+  //   // Remove margin, replace with padding
+  //   margin-left: ${theme.padding.left * -2}rem;
+  //   padding-left: ${theme.padding.left * 2}rem;
+
+  //   margin-right: ${theme.padding.left * -2}rem;
+  //   padding-right: ${theme.padding.left * 2}rem;
+
+  //   clip-path: polygon(0 0, 100% 2rem, 100% calc(100% - 2rem), 0% 100%);
+  // }
+  // & + *:not(&) {
+  //   &,
+  //   * {
+  //     margin-top: ${theme.padding.top * 2}rem;
+  //   }
+  // }
+`;
+
+export default OrdinaryDaysSection;
--- src/pages/shows/ordinary-days/index.tsx
+++ src/pages/shows/ordinary-days/index.tsx
@@ -2,22 +2,65 @@ import * as React from "react";
 
 import { styled } from "styled-components";
 import { theme } from "../components/.theme";
+import { StaticImage } from "gatsby-plugin-image";
+import OrdinaryDaysSection from "./components/OrdinaryDaysSection";
+import Logo from "./data/images/ODLogo.png";
+
+import Welcome from "./data/welcome.md";
+import AboutTheShow from "./data/aboutTheShow.md";
+import PerformanceInfo from "./data/performance.mdx";
+import Acknowledgements from "./data/acknowlegements.md";
+import Biographies from "./data/biographies.mdx";
+import AboutTorch from "./data/aboutTorch.md";
 
 interface IOrdyDaysProgrammeProps {}
 
 const OrdinaryDaysMain = styled.div`
   padding: ${theme.doublePadding};
-  background: black;
-  background-image: url("./data/images/brick.png");
+  background: ${theme.foreground};
   * {
-    color: ${theme.foreground};
+    color: ${theme.background};
   }
 `;
 
 const OrdinaryDaysProgramme: React.FunctionComponent<
   IOrdyDaysProgrammeProps
 > = (props) => {
-  return <OrdinaryDaysMain>Hey</OrdinaryDaysMain>;
+  return (
+    <OrdinaryDaysMain>
+      <img
+        src={Logo}
+        alt="Ordinary Days Logo"
+        style={{ marginTop: "4.5rem", marginBottom: "4.5rem" }}
+      />
+      <OrdinaryDaysSection>
+        <Welcome />
+      </OrdinaryDaysSection>
+      <OrdinaryDaysSection>
+        <AboutTheShow />
+      </OrdinaryDaysSection>
+      <OrdinaryDaysSection>
+        <img
+          src={Logo}
+          alt="Ordinary Days Logo"
+          style={{ marginTop: "4.5rem", marginBottom: "4.5rem" }}
+        />
+      </OrdinaryDaysSection>
+      <OrdinaryDaysSection>
+        <PerformanceInfo />
+      </OrdinaryDaysSection>
+      <OrdinaryDaysSection>
+        <Biographies />
+      </OrdinaryDaysSection>
+
+      <OrdinaryDaysSection>
+        <AboutTorch />
+      </OrdinaryDaysSection>
+      <OrdinaryDaysSection>
+        <Acknowledgements />
+      </OrdinaryDaysSection>
+    </OrdinaryDaysMain>
+  );
 };
 
 export default OrdinaryDaysProgramme;
